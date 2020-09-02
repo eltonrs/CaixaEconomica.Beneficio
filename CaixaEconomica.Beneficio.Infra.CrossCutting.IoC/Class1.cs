fdb04e265c6b19740d21ba7688a1c22d4000812a
eltonrs <elton@PC-ERS>
2020-09-02 16:09:15 -0300

    Add project files.

---
+++ CaixaEconomica.Beneficio.Infra.CrossCutting.IoC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CaixaEconomica.Beneficio.Infra.CrossCutting.IoC
+{
+  public class Class1
+  {
+  }
+}
---
+++ CaixaEconomica.Beneficio.Testes.Dominio/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CaixaEconomica.Beneficio.Testes
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
