2d0ccfa46790a1555a0e43a1a3a353fa4ca8fd8c
eltonrs <elton@PC-ERS>
2020-09-02 18:22:49 -0300

    Implementação inicial de Entidade (domínio).

---
+++ CaixaEconomica.Beneficio.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaixaEconomica.Beneficio.Dominio.Entidades
+{
+  /* Leitura:
+   * É uma classe que não pode ser instanciada, apenas herdada.
+   */
+  public abstract class Entidade
+  {
+  }
+}
